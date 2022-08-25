299f79ef8a233a3a00d50f01794d1938ba62c236
Sevenhero <sven.kernke@yahoo.de>
2022-08-26 00:10:57 +0200

    added eslint and some plugins for linting + config

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,9 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+import Head from 'next/head';
+
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
   return (
@@ -15,25 +16,31 @@ export default function Post({ postData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: postData.contentHtml,
+          }}
+        />
       </article>
     </Layout>
-  )
+  );
 }
 
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
+export function getStaticPaths() {
+  const paths = getAllPostIds();
+
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+export async function getStaticProps({ params: parameters }) {
+  const postData = await getPostData(parameters.id);
+
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
